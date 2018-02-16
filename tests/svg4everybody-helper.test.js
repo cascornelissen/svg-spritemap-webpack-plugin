31c54f25b89421dce9c26223d53b3f09c2376c5b
Cas Cornelissen <cas.cornelissen@onefinity.io>
2018-02-16 19:28:52 +0100

    Adds tests for different entry types

--- tests/svg4everybody-helper.test.js
+++ tests/svg4everybody-helper.test.js
@@ -7,7 +7,6 @@ it('Throws when the entry to which the svg4everybody helper will be added is of 
             entry: () => {},
             plugins: [
                 new SVGSpritemapPlugin({
-                    src: 'src/**/*.svg',
                     svg4everybody: true
                 })
             ]
@@ -15,6 +14,43 @@ it('Throws when the entry to which the svg4everybody helper will be added is of 
     }).toThrow();
 });
 
-// TODO: Test for entry of type string
-// TODO: Test for entry of type array
-// TODO: Test for entry of type object
+it('Supports an entry type of \'string\'', () => {
+    expect(() => {
+        webpack({
+            entry: './webpack/index.js',
+            plugins: [
+                new SVGSpritemapPlugin({
+                    svg4everybody: true
+                })
+            ]
+        });
+    }).not.toThrow();
+});
+
+it('Supports an entry type of \'array\'', () => {
+    expect(() => {
+        webpack({
+            entry: ['./webpack/index.js'],
+            plugins: [
+                new SVGSpritemapPlugin({
+                    svg4everybody: true
+                })
+            ]
+        });
+    }).not.toThrow();
+});
+
+it('Supports an entry type of \'object\'', () => {
+    expect(() => {
+        webpack({
+            entry: {
+                test: ['./webpack/index.js']
+            },
+            plugins: [
+                new SVGSpritemapPlugin({
+                    svg4everybody: true
+                })
+            ]
+        });
+    }).not.toThrow();
+});
