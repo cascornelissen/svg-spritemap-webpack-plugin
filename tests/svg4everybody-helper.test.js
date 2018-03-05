2be112c6e38682f255baebce239e61e7bb4e6a9c
Cas Cornelissen <cas.cornelissen@onefinity.io>
2018-03-05 23:06:36 +0100

    Adds test for unsupported sub-entry types

--- tests/svg4everybody-helper.test.js
+++ tests/svg4everybody-helper.test.js
@@ -1,19 +1,6 @@
 import webpack from 'webpack';
 import SVGSpritemapPlugin from '../lib/';
 
-it('Throws when the entry to which the svg4everybody helper will be added is of an unsupported type', () => {
-    expect(() => {
-        webpack({
-            entry: () => {},
-            plugins: [
-                new SVGSpritemapPlugin({
-                    svg4everybody: true
-                })
-            ]
-        });
-    }).toThrow();
-});
-
 it('Supports an entry type of \'string\'', () => {
     expect(() => {
         webpack({
@@ -44,7 +31,7 @@ it('Supports an entry type of \'object\'', () => {
     expect(() => {
         webpack({
             entry: {
-                test: ['./webpack/index.js']
+                a: ['./webpack/index.js']
             },
             plugins: [
                 new SVGSpritemapPlugin({
@@ -53,4 +40,32 @@ it('Supports an entry type of \'object\'', () => {
             ]
         });
     }).not.toThrow();
+});
+
+it('Throws when the entry to which the svg4everybody helper will be added is of an unsupported type', () => {
+    expect(() => {
+        webpack({
+            entry: () => {},
+            plugins: [
+                new SVGSpritemapPlugin({
+                    svg4everybody: true
+                })
+            ]
+        });
+    }).toThrow();
+});
+
+it('Throws when a sub-entry to which the svg4everybody helper will be added is of an unsupported type', () => {
+    expect(() => {
+        webpack({
+            entry: {
+                a: () => {}
+            },
+            plugins: [
+                new SVGSpritemapPlugin({
+                    svg4everybody: true
+                })
+            ]
+        });
+    }).toThrow();
 });
