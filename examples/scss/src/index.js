9accf75ed450e7fad831412ad63c6ea8215c9530
Cas Cornelissen <cas.cornelissen@onefinity.io>
2018-03-21 18:37:54 +0100

    Imroves SCSS formatter

--- examples/scss/src/index.js
+++ examples/scss/src/index.js
@@ -1,1 +1,1 @@
-// This file is intentionally left blank
+require('./scss/style.scss');
--- examples/scss/webpack.config.js
+++ examples/scss/webpack.config.js
@@ -1,14 +1,25 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const SVGSpritemapPlugin = require('../../lib');
 
 module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'sass-loader']
+                })
+            }
+        ]
+    },
+
     plugins: [
+        new ExtractTextPlugin('styles.css'),
         new SVGSpritemapPlugin({
             src: 'src/**/*.svg',
-            svgo: false,
-            filename: 'spritemap.svg',
-            styles: 'styles.scss',
-            // styles: path.join(__dirname, 'src/scss/test.css')
+            styles: path.join(__dirname, 'src/scss/_sprites.scss')
         })
     ]
 }
