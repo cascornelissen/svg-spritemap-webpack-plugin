8818470ea0cdcd1fda57971c757d69d255af7151
Дутченко Олег <dutchenko.o.wezom@gmail.com>
2018-08-03 11:59:35 +0300

    `writeStylesToDisk` test case

---
+++ tests/write-styles-to-disk.test.js
@@ -0,0 +1,17 @@
+import fs from 'fs';
+import del from 'del';
+import SVGSpritemapPlugin from '../lib/';
+
+it('Should create folder that is not exist and write styles spritemap content', () => {
+    const styles = '/* test content */';
+    const type = 'dir';
+    const stylesPath = './tests/output/path-to/folder-that-is-not-exist/spritemap.scss';
+	const instance = new SVGSpritemapPlugin({
+		styles: stylesPath
+	});
+
+	del.sync('./tests/output/path-to/');
+	instance.writeStylesToDisk(styles, type);
+
+	expect(fs.readFileSync(stylesPath).toString()).toBe(styles);
+});
