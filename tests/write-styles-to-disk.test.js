1700851b46ca77ec74df1c8f52b59a31ab98fc73
Дутченко Олег <dutchenko.o.wezom@gmail.com>
2018-08-03 12:10:01 +0300

    fix indent type

--- tests/write-styles-to-disk.test.js
+++ tests/write-styles-to-disk.test.js
@@ -6,12 +6,12 @@ it('Should create folder that is not exist and write styles spritemap content', 
     const styles = '/* test content */';
     const type = 'dir';
     const stylesPath = './tests/output/path-to/folder-that-is-not-exist/spritemap.scss';
-	const instance = new SVGSpritemapPlugin({
-		styles: stylesPath
-	});
+    const instance = new SVGSpritemapPlugin({
+        styles: stylesPath
+    });
 
-	del.sync('./tests/output/path-to/');
-	instance.writeStylesToDisk(styles, type);
+    del.sync('./tests/output/path-to/');
+    instance.writeStylesToDisk(styles, type);
 
-	expect(fs.readFileSync(stylesPath).toString()).toBe(styles);
+    expect(fs.readFileSync(stylesPath).toString()).toBe(styles);
 });
