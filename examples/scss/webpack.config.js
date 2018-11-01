d7c9be94305fc39e3f24d0fb7c457f55eed2682e
Cas Cornelissen <cas.cornelissen@freshheads.com>
2018-11-01 13:33:14 +0100

    Cleans up scss example configuration

--- examples/scss/webpack.config.js
+++ examples/scss/webpack.config.js
@@ -13,27 +13,10 @@ module.exports = {
             ]
         }]
     },
-
     plugins: [
         new MiniCssExtractPlugin('styles.css'),
         new SVGSpritemapPlugin('src/**/*.svg', {
-            output: {
-                // filename: path.join(svgSpritesPublicPath, 'sprites.svg'),
-                svg4everybody: true,
-            },
-            sprite: {
-                prefix: false,
-                generate: {
-                    use: true,
-                    view: '-VIEWPOSTFIX',
-                    symbol: '-SYMBOLPOSTFIX',
-                },
-            },
-            styles: {
-                format: 'fragment',
-                // filename: '~sprites.scss',
-                filename: path.join(__dirname, 'src/scss/_sprites.scss')
-            },
+            styles: path.join(__dirname, 'src/scss/_sprites.scss')
         })
     ]
 };
